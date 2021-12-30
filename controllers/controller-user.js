b616c0104bb2a17f53f9c5dc25722cf352086327
nugrahakiat <nugrahakiatsaputra@gmail.com>
2021-12-30 21:54:47 +0700

    create register controller

--- controllers/controller-user.js
+++ controllers/controller-user.js
@@ -12,6 +12,7 @@ class ControllerUser{
             res.status(500).json(error)
         })
     }
+    
     static checkLogin(req, res, next) {
         console.log(req.params.email);
         modelsUser.checkLogin(req.params.email)
@@ -20,6 +21,45 @@ class ControllerUser{
         }).catch((err) => {
             res.status(500).json(error)
         });
+    }
+
+    static register(req, res) {
+        let {nik, name, email, pob, dob, gender, phoneNumber, address, password} = req.body
+    
+        password = hashPassword(password)
+        
+        let data = {nik, name, email, pob, dob, gender, phoneNumber, address, password}
+    
+        modelsUser.register(data)
+            .then((user) => {
+                res.status(201).json({message:"success register parent"})
+            })
+            .catch((err) => {
+                res.status(500).json({message: 'error'})
+            })
+    }
+
+    static login(req, res, next) {
+        let {phoneNumber, password} = req.body
+        modelsUser.login(phoneNumber)
+            .then((user) => {
+                if(user) {
+                    if(compareHash(password, user.password)) {
+                        let payload = {
+                            id: user._id
+                        }
+                        const access_token = jwt.sign(payload, process.env.JWT_SECRET)
+                        res.status(200).json({message: "login success", access_token})
+                    } else {
+                        res.status(401).json({error:"Phone number/password is wrong"})
+                    }
+                } else {
+                    res.status(401).json({error:"Phone number not registered"})
+                }
+            })
+            .catch((error) => {
+                res.status(500).json({error: error})
+            })
     }
 }
 
